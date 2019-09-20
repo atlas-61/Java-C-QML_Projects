37ced56e23d6fb592e175e239e2defa066525071
Mümtaz <54168780+atlas-61@users.noreply.github.com>
2019-09-21 01:20:33 +0300

    Netbeans Project

---
+++ Java Calculator/src/xsanca02/TestCal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xsanca02;
+
+/**
+ *
+ * @author mumtaz
+ */
+public class TestCal {
+    public static void main(String[] args){
+
+        CalGUI My_GUI = new CalGUI();
+        My_GUI.setVisible(true);
+        
+    }  
+}
+
